395b25ce9e6adf160f7264eba2cab5dedb926741
Valerie Remaker <valerie@D-10-19-166-202.dhcp4.washington.edu>
2019-04-10 22:12:17 -0700

    everything but money

--- main.kts
+++ main.kts
@@ -1,19 +1,92 @@
 // Explore a simple class
-
-println("UW Homework: Simple Kotlin")
+     println("UW Homework : Simple Kotlin")
 
 // write a "whenFn" that takes an arg of type "Any" and returns a String
+fun whenFn(x: Any) : String {
+    when(x) {
+        is Int -> return inRange(x)
+        is String -> return isHello(x)
+        else -> return("I don't understand")
+    }
+}
+
+fun inRange(x: Int) : String {
+    if(x == 0)
+        return("zero")
+    else if(x == 1)
+        return("one")
+    else if(x in 2..10)
+        return("low number")
+    else 
+        return("a number")
+}
+
+fun isHello(x: String) : String {
+    if (x == "Hello")
+        return("world")
+    else 
+        return("Say what?")
+}
 
 // write an "add" function that takes two Ints, returns an Int, and adds the values
+fun add(x: Int, y: Int) : Int {
+    return x + y
+}
 // write a "sub" function that takes two Ints, returns an Int, and subtracts the values
+fun sub(x: Int, y: Int) : Int {
+    return x -y
+}
 // write a "mathOp" function that takes two Ints and a function (that takes two Ints and returns an Int), returns an Int, and applies the passed-in-function to the arguments
-
+fun mathOp(x: Int, y: Int, math: (Int, Int) -> Int): Int {
+    return math(x,y)
+}
 // write a class "Person" with first name, last name and age
+class Person(val firstName: String, val lastName: String, var age: Int) {
+    val debugString: String
+        get() = "[Person firstName:${firstName} lastName:${lastName} age:${age}]"
+    
+    override fun hashCode(): Int {
+        return age;
+    }
 
+    fun equals(compare: Person) : Boolean {
+        return (compare.hashCode() == this.hashCode())
+    }
+}
 // write a class "Money"
+class Money(amount: Int, currency: String) {
+    /*SET STARTING EXCPTIONS  */
+     init {
+        if (currency != "USD" && currency != "EUR" &&
+                currency != "CAN" && currency != "GBP") {
+            throw Exception("Currency not supported!")
+        } else if (amount < 0) {
+            throw Exception("Amount can't be less than zero!")
+        }
+    }
 
+    fun convert (convertMe: String) : Money {
+         when(convertMe) {
+             "tenUSD" -> return Money(5, "GBP")
+             "fiveGBP" -> return Money(10, "USD")
+             "tenUSD" -> return Money(15, "EUR")
+             else -> return Money(0, "GBP")8
+
+         }
+        /* 10 USD converts to 15 EUR; 12 USD converts to 15 CAN*/
+    } 
+}
+/*
+   "Amount" is a standard Int.
+   Define the properties such that "amount" can never be less than zero, and that "currency"
+    can only be one of those four symbols. Define a public method, convert, that takes a String 
+    argument for the currency type to convert to, and return a new Money instance with the amount 
+    converted. Conversion rates should be as follows: 10 USD converts to 5 GBP; 10 USD converts to 
+    15 EUR; 12 USD converts to 15 CAN. (Make sure you can convert in both directions!) Define the 
+    "+" operator on Money to return a new instance of Money that adds the amount, converting the 
+    currency to the first (left-hand) Money's currency. So adding (10 USD) + (5 GBP) should return a
+     result in USD. Similarly, adding (5 GBP) + (10 USD) should return the result in GBP. */
 // ============ DO NOT EDIT BELOW THIS LINE =============
-
 print("When tests: ")
 val when_tests = listOf(
     "Hello" to "world",
@@ -25,7 +98,8 @@ val when_tests = listOf(
     9 to "low number",
     17.0 to "I don't understand"
 )
-for ((k,v) in when_tests) {
+ 
+ for ((k,v) in when_tests) {
     print(if (whenFn(k) == v) "." else "!")
 }
 println("")
@@ -60,7 +134,6 @@ print(if (mathOp(2, 2, ::add ) == 4) "." else "!")
 print(if (mathOp(2, 2, ::sub ) == 0) "." else "!")
 print(if (mathOp(2, 2, { l,r -> l*r} ) == 4) "." else "!")
 println("")
-
 
 print("Person tests: ")
 val p1 = Person("Ted", "Neward", 47)
